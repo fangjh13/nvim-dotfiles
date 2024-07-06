21bf126184afe2dd660f7d1520f419c309827000
Fython <fython@spacemiracle.local>
2024-07-06 17:26:35 +0800

    add new plugin

---
+++ lua/plugins/share.lua
@@ -0,0 +1,20 @@
+return {
+  "krivahtoo/silicon.nvim",
+  build = "./install.sh",
+  config = function()
+    require("silicon").setup {
+      font = "Hack Nerd Font=26",
+      background = "#87f",
+      theme = "Monokai Extended",
+      watermark = {
+        text = "@Fython",
+      },
+      line_number = true,
+      pad_vert = 80,
+      pad_horiz = 50,
+      window_title = function()
+        return vim.fn.fnamemodify(vim.fn.bufname(vim.fn.bufnr()), ":~:.")
+      end,
+    }
+  end,
+}
