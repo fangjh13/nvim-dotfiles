3685c376ba1737bb3d5c6b7fd7fb597881aef43c
Fython <fang.jia.hui123@gmail.com>
2025-08-18 16:23:44 +0800

    optimize CopyFilePath command

--- after/plugin/commands.lua
+++ after/plugin/commands.lua
@@ -1,14 +1,29 @@
--- command `CopyFilePath` to copy current file path to clipboard
-vim.api.nvim_create_user_command("CopyFilePath", function()
-  -- "let @+ = expand('%:p')"
+-- command `CopyFilePath` to copy current file path and line number to clipboard
+vim.api.nvim_create_user_command("CopyFilePath", function(opts)
+  -- Determine whether to use an absolute or relative path
+  local file_path
+  if opts.args and (opts.args == "abs" or opts.args == "absolute") then
+    -- Use the absolute path if the argument is "abs" or "absolute"
+    file_path = vim.fn.expand "%:p"
+  else
+    -- Default to the relative path
+    file_path = vim.fn.expand "%:."
+  end
 
-  -- Get the full path of the current file
-  local file_path = vim.fn.expand "%:p"
-  -- Set the clipboard to the file path
-  vim.fn.setreg("+", file_path)
+  local line_number = vim.fn.line "."
+  local result_string = file_path .. ":" .. line_number
 
-  vim.notify("Copied file path to clipboard: " .. file_path, vim.log.levels.INFO)
-end, { nargs = 0, desc = "Copy current file path to clipboard" })
+  vim.fn.setreg("+", result_string)
+
+  vim.notify("Copied to clipboard: " .. result_string, vim.log.levels.INFO)
+end, {
+  nargs = "?", -- Allow zero or one argument
+  desc = "Copy file path and line. Pass 'abs' for absolute path.",
+  complete = function(arglead, cmdline, cursorpos)
+    -- Provide simple autocompletion for the "abs" argument
+    return { "abs", "absolute" }
+  end,
+})
 
 -- reopen the most recently closed buffer
 vim.cmd [[
